78289e2814b6f66f3cb04c7dac9d581be6da6538
carlosngv99 <carlosngv99@hotmail.com>
2020-05-31 16:45:13 -0600

    first commit

---
+++ sparse_matrix/matrix.cpp
@@ -0,0 +1,91 @@
+//
+//  matrix.cpp
+//  sparse_matrix
+//
+//  Created by Carlos NG on 5/30/20.
+//  Copyright © 2020 Carlos NG. All rights reserved.
+//
+
+#include "matrix.hpp"
+#include <iostream>
+
+using namespace std;
+
+void Matrix::add(int x, int y, int value)
+{
+    addX(x);
+    addY(y);
+    insert(x, y, value);
+}
+
+void Matrix::addX(int x)
+{
+    Node* p;
+    p = header;
+    if(p -> getRight() == NULL)
+    {
+        Node* q = new Node(x);
+        p -> setRight(q);
+        q -> setLeft(p);
+    }
+    else
+    {
+        while(p -> getRight() != NULL && p -> getRight() -> getId() < x )
+        {
+            p = p -> getRight();
+        }
+        if(p -> getRight() == NULL && p -> getId() != x)
+        {
+            Node* q = new Node(x);
+            p -> setRight(q);
+            q -> setLeft(p);
+        }
+        else if (p -> getRight() != NULL && p -> getRight() -> getId() != x)
+        {
+            Node* r = p -> getRight();
+            Node* q = new Node(x);
+            p -> setRight(q);
+            q -> setLeft(p);
+            q -> setRight(r);
+            r -> setLeft(q);
+        }
+    }
+}
+
+void Matrix::addY(int y)
+{
+    Node* p = header;
+    if(p -> getUp() == NULL)
+    {
+        Node* q = new Node(y);
+        p -> setDown(q);
+        q -> setUp(p);
+    }
+    else
+    {
+        while(p -> getDown() != NULL && p -> getDown() -> getId() < y)
+        {
+            p = p -> getDown();
+        }
+        if(p -> getDown() == NULL && p -> getId() != y)
+        {
+            Node* q = new Node(y);
+            p -> setDown(q);
+            q -> setUp(p);
+        }
+        if(p -> getDown() != NULL && p -> getDown() -> getId() != y)
+        {
+            Node* r = p -> getDown();
+            Node* q = new Node(y);
+            p -> setDown(q);
+            q -> setUp(p);
+            q -> setDown(r);
+            r -> setUp(q);
+        }
+    }
+}
+
+void Matrix::insert(int x, int y, int value)
+{
+    
+}
---
+++ sparse_matrix/matrix.hpp
@@ -0,0 +1,77 @@
+//
+//  matrix.hpp
+//  sparse_matrix
+//
+//  Created by Carlos NG on 5/30/20.
+//  Copyright © 2020 Carlos NG. All rights reserved.
+//
+
+#ifndef matrix_hpp
+#define matrix_hpp
+
+#include <stdio.h>
+
+class Node;
+class Matrix;
+
+class Node {
+private:
+    int id;
+    Node* right;
+    Node* left;
+    Node* up;
+    Node* down;
+public:
+    Node(int id){
+        this -> id = id;
+        right = NULL;
+        left = NULL;
+        up = NULL;
+        down = NULL;
+    };
+    Node* getRight(){
+        return right;
+    };
+    Node* getLeft(){
+        return left;
+    };
+    Node* getUp(){
+        return up;
+    };
+    Node* getDown(){
+        return down;
+    };
+    void setRight(Node* value){
+        right = value;
+    }
+    void setLeft(Node* value){
+        left = value;
+    }
+    void setUp(Node* value){
+        up = value;
+    }
+    void setDown(Node* value){
+        down = value;
+    }
+    int getId()
+    {
+        return id;
+    }
+};
+
+class Matrix
+{
+private:
+    Node* header;
+public:
+    Matrix()
+    {
+        header = new Node(0);
+    };
+    void add(int,int,int);
+    void addX(int);
+    void addY(int);
+    void insert(int,int,int);
+};
+
+#endif /* matrix_hpp */
